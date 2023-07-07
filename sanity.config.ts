491f9c6f346782bc5e44a04648d5f6e09c6f2f26
Falase Femi <femifalase228@gmail.com>
2023-07-07 17:47:17 +0100

    sanity desk in next js

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,15 @@
-import {defineConfig} from "sanity"
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
-    projectId: "yf3pjenc",
-    dataset: "production",
-    title: "My Personal Website",
-    apiVersion: "2023-07-07",
-    basePath: "/admin"
-})
+  projectId: "yf3pjenc",
+  dataset: "production",
+  title: "My Personal Website",
+  apiVersion: "2023-07-07",
+  basePath: "/admin",
+  plugins: [deskTool()],
+  schema: { types: schemas },
+});
 
 export default config;
